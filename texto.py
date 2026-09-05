f1c593b1f538ae6169416944966fd0899fec0ef2
anaepm <perezmanae@gmail.com>
2026-09-05 07:10:25 -0600

    Update texto.py

--- texto.py
+++ texto.py
@@ -2,14 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+# 1. Peticion web
 url = "http://books.toscrape.com/"
 response = requests.get(url)
 response.encoding = 'utf-8'
 
+# 2. Parsing de HTML
 soup = BeautifulSoup(response.text, "html.parser")
 libros = soup.find_all("article", class_="product_pod")
 
 datos = []
+
+# 3. Extraccion de datos
 for libro in libros:
     titulo = libro.h3.a["title"]
     precio_texto = libro.find("p", class_="price_color").text
@@ -22,6 +26,7 @@ for libro in libros:
         "disponibilidad": disponibilidad
     })
 
+# 4. Creacion y exportacion del CSV
 df = pd.DataFrame(datos)
 df.to_csv("catalogo_libros.csv", index=False)
-print("Scraping exitoso y archivo catalogo_libros.csv creado.")
+print("Scraping ejecutado con exito y catalogo_libros.csv generado.")
